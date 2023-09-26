1ca2b40acc2a49893345253d9c4d8f30e703267c
KenzaElHarrif <112128295+KenzaElHarrif@users.noreply.github.com>
2023-09-26 13:46:43 -0400

    Ajout des boutons clés

--- script.js
+++ script.js
@@ -1,79 +1,104 @@
 chapters = {
     debut :{
-        titre: ``,
-        description:``,
-        image: ``,
-        bouton: [],
+        titre: '',
+        description:'',
+        image: '',
+        bouton: [
+            {titre: 'Continuer','destination': 'activation',}
+        ],
     },
 
     activation: {
-        titre: ``,
-        description:``,
-        image: ``,
-        bouton: [],
+        titre: '',
+        description:'',
+        image: '',
+        bouton: [
+            {titre: 'Attaquer les robots','destination': 'robot',},
+            {titre: 'Continuer son chemin','destination': 'blackbox',}
+        ],
     },
 
-    attaqueRobot: {
-        titre: ``,
-        description:``,
-        image: ``,
-        bouton: [],
+    robot: {
+        titre: '',
+        description:'',
+        image: '',
+        bouton: [
+            {titre: 'Continuer son chemin','destination': 'blackbox',}
+        ],
     },
 
     blackbox:{
-        titre: ``,
-        description:``,
-        image: ``,
-        bouton: [],
+        titre: '',
+        description:'',
+        image: '',
+        bouton: [
+            {titre: 'Retrouver les blackbox','destination': 'simone',},
+            {titre: 'Continuer son chemin','destination': 'verite',}],
     },
 
     verite:{
-        titre: ``,
-        description:``,
-        image: ``,
-        bouton: [],
+        titre: '',
+        description:'',
+        image: '',
+        bouton: [
+            {titre: 'Retour au début','destination': 'debut',},
+        ],
     },
 
-    chanteuseRobot:{
-        titre: ``,
-        description:``,
-        image: ``,
-        bouton: [],
+    simone:{
+        titre: '',
+        description:'',
+        image: '',
+        bouton: [
+            {titre: 'Attaque frontale','destination': 'marionettes',},
+            {titre: 'Attaque furtive','destination': 'ennemidown',}/*COMMENT ARRIVER AU CHAPITRE 4 ?? */
+        ],
     },
 
     marionettes:{
-        titre: ``,
-        description:``,
-        image: ``,
-        bouton: [],
+        titre: '',
+        description:'',
+        image: '',
+        bouton: [
+            {titre: 'Retour au début','destination': 'debut',},
+        ],
     },
 
-    ennemiDown:{
-        titre: ``,
-        description:``,
-        image: ``,
-        bouton: [],
+    ennemidown:{
+        titre: '',
+        description:'',
+        image: '',
+        bouton: [
+            {titre: 'Retour au début','destination': 'debut',},
+        ],
     },
 
     choix:{
-        titre: ``,
-        description:``,
-        image: ``,
-        bouton: [],
+        titre: '',
+        description:'',
+        image: '',
+        bouton: [
+            {titre: 'Sauver 2S','destination': 'scanner',},
+            {titre: 'Sauver 1N','destination': 'nier',},
+        ],
     },
 
     scanner:{
-        titre: ``,
-        description:``,
-        image: ``,
-        bouton: [],
+        titre: '',
+        description:'',
+        image: '',
+        bouton: [
+            {titre: 'Retour au début','destination': 'debut',},
+        ],
     },
 
     nier:{
-        titre: ``,
-        description:``,
-        image: ``,
-        bouton: [],
+        titre: '',
+        description:'',
+        image: '',
+        bouton: [
+            {titre: 'Retour au début','destination': 'debut',},
+        ],
     },
-    
+
 }
