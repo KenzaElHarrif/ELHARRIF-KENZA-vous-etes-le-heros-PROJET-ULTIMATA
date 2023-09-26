f68f52116e4744cf9a932af0c97ef7750045ada7
KenzaElHarrif <112128295+KenzaElHarrif@users.noreply.github.com>
2023-09-26 13:05:12 -0400

    ajout des chapitres dans le code

--- script.js
+++ script.js
@@ -1,6 +1,79 @@
-let chapitrePremier = {
-    titre: "Gloire à l'humanité",
-    description: "Mon début d'histoire",
-    image: "URL",
-    boutons: [],
+chapters = {
+    debut :{
+        titre: ``,
+        description:``,
+        image: ``,
+        bouton: [],
+    },
+
+    activation: {
+        titre: ``,
+        description:``,
+        image: ``,
+        bouton: [],
+    },
+
+    attaqueRobot: {
+        titre: ``,
+        description:``,
+        image: ``,
+        bouton: [],
+    },
+
+    blackbox:{
+        titre: ``,
+        description:``,
+        image: ``,
+        bouton: [],
+    },
+
+    verite:{
+        titre: ``,
+        description:``,
+        image: ``,
+        bouton: [],
+    },
+
+    chanteuseRobot:{
+        titre: ``,
+        description:``,
+        image: ``,
+        bouton: [],
+    },
+
+    marionettes:{
+        titre: ``,
+        description:``,
+        image: ``,
+        bouton: [],
+    },
+
+    ennemiDown:{
+        titre: ``,
+        description:``,
+        image: ``,
+        bouton: [],
+    },
+
+    choix:{
+        titre: ``,
+        description:``,
+        image: ``,
+        bouton: [],
+    },
+
+    scanner:{
+        titre: ``,
+        description:``,
+        image: ``,
+        bouton: [],
+    },
+
+    nier:{
+        titre: ``,
+        description:``,
+        image: ``,
+        bouton: [],
+    },
+    
 }
