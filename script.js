1c39e976a30e036547ac545763f818df76de9713
KenzaElHarrif <112128295+KenzaElHarrif@users.noreply.github.com>
2023-09-26 22:51:37 -0400

    ajout fonction

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
+const simoneDestiInit= 'ennemidown';
+const simoneDestiRobot = 'choix';
+
+simoneDesti = simoneDestiInit;
+
+
 chapters = {
     debut :{
-        titre: 'test 1',
-        description:' texte 1',
-        image: '',
+        titre: `Gloire à l’humanité`,
+        description:`Il y a plusieurs années auparavant, les extraterrestres ont mis fin à la grande ère de l’humanité. Peu après leur arrivée sur Terre, ils ont lâché leur plus grande arme, des robots. Les humains n’ont trouvé comme solution que d’aller se réfugier sur la lune. Une dizaine d’années plus tard, YoRha fut créé afin de repousser l’ennemi et aider l’humanité à reprendre leur planète. Malheureusement, cela fait bien longtemps que nous nous battons. 
+        \nC’est pour cette raison que vous, soldat, avez pour mission de vous battre en l’honneur de l’humanité. Afin de reprendre ce qu’appartient à nos créateurs. Nous avons confiance en vous. `,
+        image: ``,
         bouton: [
             {titre: 'Continuer','destination': 'activation',}
         ],
     },
 
     activation: {
-        titre: 'test 2',
-        description:'text 2',
-        image: '',
+        titre: `test2`,
+        description:`texte2`,
+        image: ``,
         bouton: [
             {titre: 'Attaquer les robots','destination': 'robot',},
             {titre: 'Continuer son chemin','destination': 'blackbox',}
@@ -19,64 +26,64 @@ chapters = {
     },
 
     robot: {
-        titre: '',
-        description:'',
-        image: '',
+        titre: ``,
+        description:``,
+        image: ``,
         bouton: [
             {titre: 'Continuer son chemin','destination': 'blackbox',}
         ],
     },
 
     blackbox:{
-        titre: '',
-        description:'',
-        image: '',
+        titre: ``,
+        description:``,
+        image: ``,
         bouton: [
             {titre: 'Retrouver les blackbox','destination': 'simone',},
             {titre: 'Continuer son chemin','destination': 'verite',}],
     },
 
     verite:{
-        titre: '',
-        description:'',
-        image: '',
+        titre: ``,
+        description:``,
+        image: ``,
         bouton: [
             {titre: 'Retour au début','destination': 'debut',},
         ],
     },
 
     simone:{
-        titre: '',
-        description:'',
-        image: '',
+        titre: ``,
+        description:``,
+        image: ``,
         bouton: [
             {titre: 'Attaque frontale','destination': 'marionettes',},
-            {titre: 'Attaque furtive','destination': 'ennemidown',}/*COMMENT ARRIVER AU CHAPITRE 4 ?? */
+            {titre: 'Attaque furtive','destination': simoneDesti,}/*COMMENT ARRIVER AU CHAPITRE 4 ?? */
         ],
     },
 
     marionettes:{
-        titre: '',
-        description:'',
-        image: '',
+        titre: ``,
+        description:``,
+        image: ``,
         bouton: [
             {titre: 'Retour au début','destination': 'debut',},
         ],
     },
 
     ennemidown:{
-        titre: '',
-        description:'',
-        image: '',
+        titre: ``,
+        description:``,
+        image: ``,
         bouton: [
             {titre: 'Retour au début','destination': 'debut',},
         ],
     },
 
     choix:{
-        titre: '',
-        description:'',
-        image: '',
+        titre: ``,
+        description:``,
+        image: ``,
         bouton: [
             {titre: 'Sauver 2S','destination': 'scanner',},
             {titre: 'Sauver 1N','destination': 'nier',},
@@ -84,21 +91,45 @@ chapters = {
     },
 
     scanner:{
-        titre: '',
-        description:'',
-        image: '',
+        titre: ``,
+        description:``,
+        image: ``,
         bouton: [
             {titre: 'Retour au début','destination': 'debut',},
         ],
     },
 
     nier:{
-        titre: '',
-        description:'',
-        image: '',
+        titre: ``,
+        description:``,
+        image: ``,
         bouton: [
             {titre: 'Retour au début','destination': 'debut',},
         ],
     },
 
 }
+
+function goToChapter(chapter){
+        if(chapters[chapter]){
+            if(chapter == 'robot'){
+                chapters.simone.bouton[1].destination = simoneDestiRobot;
+            }
+
+
+            console.log(chapters[chapter].titre);
+            console.log(chapters[chapter].description);
+            console.log("Options:")
+
+            chapters[chapter].bouton.forEach( function (bout){
+                console.log(`${bout.titre} \ntapez: goToChapter(\'${bout.destination}\')`  );
+            });
+        }
+        
+        else{
+            console.log(`Attention! C'est la mauvaise clé.`);
+        }
+        return 'Pod A attend votre décision...-..-.-..-';
+}
+
+goToChapter('debut');
